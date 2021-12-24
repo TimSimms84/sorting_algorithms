e845bdb26020c71270d8b1360379e7d70cc975e6
Tim <tsimms@gmail.com>
2021-12-24 13:44:54 -0600

    changed to use swap function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,15 +1,26 @@
 #include "sort.h"
 /**
+ * swap - swaps two variables
+ * @i: first one to swap
+ * @j: second one to swap
+ */
+
+void swap(int *i, int *j)
+{
+	int tmp = *i;
+
+	*i = *j;
+	*j = tmp;
+}
+
+/**
  * bubble_sort - sorts an array using the bubble sort algorithm
  * @array: array to sort
  * @size: size of the array
  */
-
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i;
-	unsigned int j;
-	int tmp;
+	unsigned int i, j;
 
 	for (i = 0; i < size; i++)
 	{
@@ -17,9 +28,7 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
+				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
 			}
 		}
