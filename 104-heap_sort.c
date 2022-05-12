045ef69b649f7d7f6c1ce4d1639cc7979a6a6bab
Tim Simms <tsimms@gmail.com>
2022-05-12 18:54:21 -0500

    heap sort

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,7 +1,13 @@
 #include "sort.h"
 
-void heap_sort(__attribute__ ((unused)) int *array, __attribute__ ((unused)) size_t size)
+/**
+ * heap_sort - heap sort on array
+ * @array: array
+ * @size: size of array
+ */
+
+void heap_sort(__attribute__ ((unused)) int *array, size_t size)
 {
-
-
+	size = 0;
+	printf("%i", size);
 }
