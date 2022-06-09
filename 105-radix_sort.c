46a3ede1c024a679ccbc86c57b7443b3e6e6477d
Tim Simms <tsimms@gmail.com>
2022-06-09 16:14:28 -0500

    fixed segfault when array is NULL and called get_max

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -86,10 +86,12 @@ void countSort(int *arr, int size, int exp)
 void radix_sort(int *array, size_t size)
 {
 
-	int exp, max = get_max(array, size);
+	int exp, max;
 
 	if (!array || size < 2)
 		return;
+
+	max = get_max(array, size);
 
 	for (exp = 1; max / exp > 0; exp *= 10)
 	{
